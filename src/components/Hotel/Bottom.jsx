46b246b61bb506b5e57211941eb48b079b6ce392
Satyajeet Behera <jagajajpur3@gmail.com>
2024-06-10 11:47:58 +0530

    change

---
+++ src/components/Hotel/Bottom.jsx
@@ -0,0 +1,286 @@
+import { Bottomcss } from "./Bottomcss";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+export const Bottom = () => {
+  return (
+    <Bottomcss>
+      <div className="div1">
+        <div className="m-8">
+          <div class="p-5">
+            <h2 class="text-xl font-bold text-gray-900">PRODUCT OFFERING</h2>
+            <ul class="m-0 p-0 flex flex-wrap">
+              <li class="inline text-xs m-1">Flights</li>
+              <li class="inline text-xs m-1">International Flights</li>
+              <li class="inline text-xs m-1">Charter Flights</li>
+              <li class="inline text-xs m-1">Hotels</li>
+              <li class="inline text-xs m-1">International Hotels</li>
+              <li class="inline text-xs m-1">Homestays and Villas</li>
+              <li class="inline text-xs m-1">Activities</li>
+              <li class="inline text-xs m-1">Holidays In India</li>
+              <li class="inline text-xs m-1">International Holidays</li>
+              <li class="inline text-xs m-1">Book Hotels From UAE</li>
+              <li class="inline text-xs m-1">Book Online Cabs</li>
+              <li class="inline text-xs m-1">Book Bus Tickets</li>
+              <li class="inline text-xs m-1">Book Train Tickets</li>
+              <li class="inline text-xs m-1">Cheap Tickets to India</li>
+              <li class="inline text-xs m-1">Book Flights From US</li>
+              <li class="inline text-xs m-1">Book Flights From UAE</li>
+              <li class="inline text-xs m-1">Trip Planner</li>
+              <li class="inline text-xs m-1">Gift Cards</li>
+              <li class="inline text-xs m-1">Gift</li>
+              <li class="inline text-xs m-1">Wedding Gift</li>
+              <li class="inline text-xs m-1">Anniversary Gift</li>
+              <li class="inline text-xs m-1">Birthday Gift</li>
+              <li class="inline text-xs m-1">Diwali Gift</li>
+              <li class="inline text-xs m-1">Valentines Gift</li>
+              <li class="inline text-xs m-1">Farewell Gift</li>
+              <li class="inline text-xs m-1">Christmas Gift</li>
+              <li class="inline text-xs m-1">New Year Gift</li>
+              <li class="inline text-xs m-1">Trip Money</li>
+              <li class="inline text-xs m-1">Trip Ideas</li>
+              <li class="inline text-xs m-1">Travel Blog</li>
+              <li class="inline text-xs m-1">PNR Status</li>
+              <li class="inline text-xs m-1">
+                MakeMyTrip Advertising Solutions
+              </li>
+              <li class="inline text-xs m-1">One Way Cab</li>
+            </ul>
+          </div>
+          <div class="p-5">
+            <h2 class="text-xl font-bold text-gray-900">MAKEMYTRIP</h2>
+            <ul class="m-0 p-0 flex flex-wrap list-none">
+              <li class="text-xs m-1">About Us</li>
+              <li class="text-xs m-1">Investor Relations</li>
+              <li class="text-xs m-1">Careers</li>
+              <li class="text-xs m-1">MMT Foundation</li>
+              <li class="text-xs m-1">Legal Notices</li>
+              <li class="text-xs m-1">CSR Policy</li>
+              <li class="text-xs m-1">myPartner - Travel Agent Portal</li>
+              <li class="text-xs m-1">Foreign Exchange</li>
+              <li class="text-xs m-1">List your hotel</li>
+              <li class="text-xs m-1">Partners- Redbus</li>
+              <li class="text-xs m-1">Partners- Goibibo</li>
+              <li class="text-xs m-1">Advertise with Us</li>
+              <li class="text-xs m-1">Partners- BookMyForex</li>
+              <li class="text-xs m-1">RedBus Ferry Malaysia</li>
+              <li class="text-xs m-1">RedBus Ferry Singapore</li>
+              <li class="text-xs m-1">redBus Vietnam</li>
+              <li class="text-xs m-1">redBus Cambodia</li>
+              <li class="text-xs m-1">Things to Do in Malaysia</li>
+              <li class="text-xs m-1">Things to Do in Singapore</li>
+            </ul>
+          </div>
+          <div class="p-5">
+            <h2 class="text-xl font-bold text-gray-900">ABOUT THIS SITE</h2>
+            <ul class="m-0 p-0 flex flex-wrap list-none">
+              <li class="text-xs m-1">Customer Support</li>
+              <li class="text-xs m-1">Payment Security</li>
+              <li class="text-xs m-1">Privacy Policy</li>
+              <li class="text-xs m-1">Cookie Policy</li>
+              <li class="text-xs m-1">User Agreement</li>
+              <li class="text-xs m-1">Terms of Service</li>
+              <li class="text-xs m-1">More Offices</li>
+              <li class="text-xs m-1">Make A Payment</li>
+              <li class="text-xs m-1">Work From Home</li>
+              <li class="text-xs m-1">Escalation Channel</li>
+              <li class="text-xs m-1">Report Security Issues</li>
+            </ul>
+          </div>
+          <div class="p-5">
+            <h2 class="text-xl font-bold text-gray-900">TOP HOTELS IN INDIA</h2>
+            <ul class="m-0 p-0 flex flex-wrap list-none">
+              <li class="text-xs m-1">Fairmont Jaipur</li>
+              <li class="text-xs m-1">St Regis Goa</li>
+              <li class="text-xs m-1">Six Senses Fort Barwara</li>
+              <li class="text-xs m-1">W Goa</li>
+              <li class="text-xs m-1">Grand Hyatt Goa</li>
+              <li class="text-xs m-1">Shangri-La Bangalore</li>
+              <li class="text-xs m-1">The St Regis Mumbai</li>
+              <li class="text-xs m-1">Taj Rishikesh</li>
+              <li class="text-xs m-1">Grand Hyatt Mumbai</li>
+              <li class="text-xs m-1">Le Meridien Delhi</li>
+              <li class="text-xs m-1">Rambagh Palace Jaipur</li>
+              <li class="text-xs m-1">Leela Palace Chennai</li>
+              <li class="text-xs m-1">The Leela Palace Udaipur</li>
+              <li class="text-xs m-1">Taj Lake Palace Udaipur</li>
+              <li class="text-xs m-1">Jw Marriott Chandigarh</li>
+              <li class="text-xs m-1">Alila Diwa Goa</li>
+              <li class="text-xs m-1">Le Meridien Goa</li>
+              <li class="text-xs m-1">Taj Lands End Mumbai</li>
+              <li class="text-xs m-1">Itc Grand Chola Chennai</li>
+              <li class="text-xs m-1">Itc Maratha Mumbai</li>
+              <li class="text-xs m-1">Oberoi Udaivilas</li>
+              <li class="text-xs m-1">Jai Mahal Palace Jaipur</li>
+              <li class="text-xs m-1">Taj Mahal Tower Mumbai</li>
+              <li class="text-xs m-1">Marriott Suites Pune</li>
+              <li class="text-xs m-1">Park Hyatt Chennai</li>
+              <li class="text-xs m-1">The Leela Palace Jaipur</li>
+              <li class="text-xs m-1">Jw Marriott Mumbai Sahar</li>
+              <li class="text-xs m-1">Jw Marriott Mumbai Juhu</li>
+              <li class="text-xs m-1">The Ritz Carlton Bengaluru</li>
+              <li class="text-xs m-1">The Oberoi New Delhi</li>
+              <li class="text-xs m-1">Taj Resort & Convention Centre Goa</li>
+              <li class="text-xs m-1">Taj Bengal Kolkata</li>
+              <li class="text-xs m-1">Taj Coromandel Chennai</li>
+              <li class="text-xs m-1">The Oberoi Gurgaon</li>
+              <li class="text-xs m-1">The Westin Goa</li>
+              <li class="text-xs m-1">Jw Marriott Hotel Pune</li>
+              <li class="text-xs m-1">The Leela Palace New Delhi</li>
+              <li class="text-xs m-1">Taj West End Bengaluru</li>
+              <li class="text-xs m-1">The Taj Mahal Palace Mumbai</li>
+            </ul>
+          </div>
+          <div class="p-5">
+            <h2 class="text-xl font-bold text-gray-900">
+              TOP INTERNATIONAL HOTELS
+            </h2>
+            <ul class="m-0 p-0 flex flex-wrap list-none">
+              <li class="text-xs m-1">Adaaran Club Rannalhi</li>
+              <li class="text-xs m-1">Marina Bay Sands Singapore</li>
+              <li class="text-xs m-1">Coco Bodu Hithi</li>
+              <li class="text-xs m-1">Taj Dubai</li>
+              <li class="text-xs m-1">Atlantis Hotel Dubai</li>
+              <li class="text-xs m-1">Amari Phuket</li>
+              <li class="text-xs m-1">Jw Marriott Dubai</li>
+              <li class="text-xs m-1">Armani Hotel Dubai</li>
+              <li class="text-xs m-1">Grand Hyatt Dubai</li>
+              <li class="text-xs m-1">Saii Lagoon Maldives</li>
+              <li class="text-xs m-1">Gevora Hotel Dubai</li>
+              <li class="text-xs m-1">Hyatt Regency Dubai</li>
+              <li class="text-xs m-1">Pan Pacific Singapore</li>
+              <li class="text-xs m-1">The Palm Dubai</li>
+              <li class="text-xs m-1">Caesars Palace</li>
+              <li class="text-xs m-1">Baiyoke Sky Hotel</li>
+              <li class="text-xs m-1">Centara Pattaya Hotel</li>
+              <li class="text-xs m-1">Embudu Village</li>
+              <li class="text-xs m-1">Orchard Hotel Singapore</li>
+              <li class="text-xs m-1">Reethi Beach Resort</li>
+              <li class="text-xs m-1">Ambassador Hotel Bangkok</li>
+              <li class="text-xs m-1">Dusit Thani Pattaya</li>
+              <li class="text-xs m-1">Shangri La Singapore</li>
+              <li class="text-xs m-1">Sunbeam Hotel Pattaya</li>
+              <li class="text-xs m-1">Taj Samudra Colombo</li>
+              <li class="text-xs m-1">Bangkok Palace Hotel</li>
+              <li class="text-xs m-1">Hilton Pattaya</li>
+              <li class="text-xs m-1">Novotel Phuket Resort</li>
+              <li class="text-xs m-1">Taj Exotica Resort Maldives</li>
+              <li class="text-xs m-1">Village Hotel Bugis</li>
+              <li class="text-xs m-1">Avani Atrium Bangkok</li>
+              <li class="text-xs m-1">The Plaza New York</li>
+              <li class="text-xs m-1">Village Hotel Albert Court</li>
+              <li class="text-xs m-1">Amari Pattaya</li>
+            </ul>
+          </div>
+          <div class="p-5">
+            <h2 class="text-xl font-bold text-gray-900">QUICK LINKS</h2>
+            <ul class="m-0 p-0 flex flex-wrap list-none">
+              <li class="text-xs m-1">Delhi Chennai Flights</li>
+              <li class="text-xs m-1">Delhi Mumbai Flights</li>
+              <li class="text-xs m-1">Delhi Goa Flights</li>
+              <li class="text-xs m-1">Chennai Mumbai Flights</li>
+              <li class="text-xs m-1">Mumbai Hyderabad Flights</li>
+              <li class="text-xs m-1">Kolkata to Rupsi Flights</li>
+              <li class="text-xs m-1">Rupsi to Guwahati Flights</li>
+              <li class="text-xs m-1">Pasighat to Guwahati Flights</li>
+              <li class="text-xs m-1">Delhi to Khajuraho Flights</li>
+              <li class="text-xs m-1">Cochin to Agatti Island Flights</li>
+              <li class="text-xs m-1">Hotels in Delhi</li>
+              <li class="text-xs m-1">Hotels in Mumbai</li>
+              <li class="text-xs m-1">Hotels In Goa</li>
+              <li class="text-xs m-1">Hotels In Jaipur</li>
+              <li class="text-xs m-1">Hotels In Ooty</li>
+              <li class="text-xs m-1">Hotels In Udaipur</li>
+              <li class="text-xs m-1">Hotels in Puri</li>
+              <li class="text-xs m-1">Hotels In North Goa</li>
+              <li class="text-xs m-1">Hotels In Rishikesh</li>
+              <li class="text-xs m-1">Honeymoon Packages</li>
+              <li class="text-xs m-1">Kerala Packages</li>
+              <li class="text-xs m-1">Kashmir Packages</li>
+              <li class="text-xs m-1">Ladakh Packages</li>
+              <li class="text-xs m-1">Goa Packages</li>
+              <li class="text-xs m-1">Thailand Packages</li>
+              <li class="text-xs m-1">Sri Lanka Visa</li>
+              <li class="text-xs m-1">Thailand Visa</li>
+              <li class="text-xs m-1">Explore Goa</li>
+              <li class="text-xs m-1">Explore Manali</li>
+              <li class="text-xs m-1">Explore Shimla</li>
+              <li class="text-xs m-1">Explore Jaipur</li>
+              <li class="text-xs m-1">Explore Srinagar</li>
+            </ul>
+          </div>
+          <div class="p-5">
+            <h2 class="text-xl font-bold text-gray-900">IMPORTANT LINKS</h2>
+            <ul className="m-0 p-0 flex flex-wrap list-none">
+              <li class="text-xs m-1">Cheap Flights</li>
+              <li class="text-xs m-1">Flight Status</li>
+              <li class="text-xs m-1">Kumbh Mela</li>
+              <li class="text-xs m-1">Domestic Airlines</li>
+              <li class="text-xs m-1">International Airlines</li>
+              <li class="text-xs m-1">Indigo</li>
+              <li class="text-xs m-1">Spicejet</li>
+              <li class="text-xs m-1">GoAir</li>
+              <li class="text-xs m-1">Air Asia</li>
+              <li class="text-xs m-1">Air India</li>
+              <li class="text-xs m-1">Indian Railways</li>
+              <li class="text-xs m-1">Corporate Flight Booking</li>
+              <li class="text-xs m-1">Trip Ideas</li>
+              <li class="text-xs m-1">Beaches</li>
+              <li class="text-xs m-1">Honeymoon Destinations</li>
+              <li class="text-xs m-1">Romantic Destinations</li>
+              <li class="text-xs m-1">Popular Destinations</li>
+              <li class="text-xs m-1">Resorts In Udaipur</li>
+              <li class="text-xs m-1">Resorts In Munnar</li>
+              <li class="text-xs m-1">Villas In Lonavala</li>
+              <li class="text-xs m-1">Villas In Goa</li>
+              <li class="text-xs m-1">Domestic Flight Offers</li>
+              <li class="text-xs m-1">International Flight Offers</li>
+              <li class="text-xs m-1">UAE Flight Offers</li>
+              <li class="text-xs m-1">USA</li>
+              <li class="text-xs m-1">UAE</li>
+              <li class="text-xs m-1">Saudi Arabia</li>
+              <li class="text-xs m-1">UK</li>
+              <li class="text-xs m-1">Oman</li>
+            </ul>
+            <h2 class="text-xl font-bold text-gray-900 mt-4">
+              CORPORATE TRAVEL
+            </h2>
+            <ul className="m-0 p-0 flex flex-wrap list-none">
+              <li class="text-xs m-1">Corporate Travel</li>
+              <li class="text-xs m-1">Corporate Hotel Booking</li>
+              <li class="text-xs m-1">Corporate Flight Booking</li>
+              <li class="text-xs m-1">Business Travel for SME</li>
+              <li class="text-xs m-1">GST Invoice for International flights</li>
+              <li class="text-xs m-1">Business Travel Solutions</li>
+              <li class="text-xs m-1">GST Invoice for Bus</li>
+              <li class="text-xs m-1">Corporate Bus booking</li>
+              <li class="text-xs m-1">myBiz - Best Business Travel Platform</li>
+              <li class="text-xs m-1">GST Invoice for Flights</li>
+              <li class="text-xs m-1">GST Invoice for Corporate Travel</li>
+              <li class="text-xs m-1">GST Invoice for Hotels</li>
+              <li class="text-xs m-1">myBiz for Small Business</li>
+              <li class="text-xs m-1">
+                Free cancellation on International Flights
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="div4">
+        <div className="fb">
+          <div className="onee text-5xl">
+            <div>
+              <TwitterIcon sx={{ fontSize: 40 }} />
+            </div>
+            <div>
+              <FacebookIcon sx={{ fontSize: 40 }} />
+            </div>
+          </div>
+          <div className="copy text-right">
+            <h4>© 2024 MAKEMYTRIP PVT. LTD.</h4>
+            <h4>Country India USA UAE</h4>
+          </div>
+        </div>
+      </div>
+    </Bottomcss>
+  );
+};
